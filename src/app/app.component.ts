0916b7752ba7d5f9288cdf2c54e6dd388f000b21
Carlos Rodrigo Beltrá <carrodbel3@alu.edu.gva.es>
2026-02-23 12:53:01 +0100

    registro, login, interceptores y guardianes añadidos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,35 @@
-
-import { Component } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
-import { IonApp, IonSplitPane, IonMenu, IonContent, IonList, IonListHeader, IonNote, IonMenuToggle, IonItem, IonIcon, IonLabel, IonRouterOutlet, IonRouterLink } from '@ionic/angular/standalone';
+import { SplashScreen } from '@capacitor/splash-screen';
+import { IonApp, IonAvatar, IonContent, IonIcon, IonImg, IonItem, IonLabel, IonList, IonMenu, IonMenuToggle, IonRouterLink, IonRouterOutlet, IonSplitPane, Platform } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
-import { mailOutline, mailSharp, paperPlaneOutline, paperPlaneSharp, heartOutline, heartSharp, archiveOutline, archiveSharp, trashOutline, trashSharp, warningOutline, warningSharp, bookmarkOutline, bookmarkSharp } from 'ionicons/icons';
+import { arrowUndoCircle, camera, checkmarkCircle, documentText, home, images, logIn } from 'ionicons/icons';
+import { Auth } from './auth/services/auth.service';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
-  imports: [RouterLink, RouterLinkActive, IonApp, IonSplitPane, IonMenu, IonContent, IonList, IonListHeader, IonNote, IonMenuToggle, IonItem, IonIcon, IonLabel, IonRouterLink, IonRouterOutlet],
+  imports: [RouterLink, RouterLinkActive, IonRouterLink, IonApp, IonSplitPane, IonMenu, IonContent, IonList, IonMenuToggle, IonItem, IonIcon, IonLabel, IonRouterOutlet, IonAvatar, IonImg],
 })
 export class AppComponent {
-  public appPages = [
-    { title: 'Inbox', url: '/folder/inbox', icon: 'mail' },
-    { title: 'Outbox', url: '/folder/outbox', icon: 'paper-plane' },
-    { title: 'Favorites', url: '/folder/favorites', icon: 'heart' },
-    { title: 'Archived', url: '/folder/archived', icon: 'archive' },
-    { title: 'Trash', url: '/folder/trash', icon: 'trash' },
-    { title: 'Spam', url: '/folder/spam', icon: 'warning' },
-  ];
-  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
+  #authService = inject(Auth);
+  #platform = inject(Platform);
+
+  userResource = this.#authService.getProfile();
+  user = computed(() => this.userResource.hasValue() ? this.userResource.value().user : null);
+
+  public appPages = [{ title: 'Home', url: '/products', icon: 'home' }];
   constructor() {
-    addIcons({ mailOutline, mailSharp, paperPlaneOutline, paperPlaneSharp, heartOutline, heartSharp, archiveOutline, archiveSharp, trashOutline, trashSharp, warningOutline, warningSharp, bookmarkOutline, bookmarkSharp });
+    addIcons({ home, logIn, documentText, checkmarkCircle, images, camera, arrowUndoCircle, });
+    this.initializeApp();
+  }
+
+  async initializeApp() {
+    if (this.#platform.is('capacitor')) {
+      await this.#platform.ready();
+      SplashScreen.hide();
+    }
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,18 @@ import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalo
 
 import { AppComponent } from './app/app.component';
 import { routes } from './app/app.routes';
+import { provideHttpClient, withInterceptors } from "@angular/common/http";
+import { baseUrlInterceptor } from "./app/interceptors/base-url.interceptor";
+import { authInterceptor } from "./app/interceptors/auth.interceptor";
+import { defineCustomElements } from '@ionic/pwa-elements/loader';
+
+defineCustomElements(window);
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideZonelessChangeDetection(),{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
+    provideHttpClient(withInterceptors([baseUrlInterceptor, authInterceptor])),
     provideRouter(routes, withPreloading(PreloadAllModules)),
   ],
 });
